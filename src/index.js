9798d16d6545a68e871e2d9c481101e5e4e89468
Tiago <tiago.santos@ayoapp.com.br>
2020-01-28 11:33:07 -0300

    adiciona cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors')
 
 mongoose.connect('mongodb+srv://oministack:wehkyp-3quxna-ticNeb@caricadevelopment-mzdkg.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://oministack:wehkyp-3quxna-ticNeb@caricadevelopmen
 });
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
